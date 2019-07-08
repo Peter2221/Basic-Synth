dde9e89b6010ec75e9d0f8f573538b9b4d3c0196
Peter2221 <pmbasen@gmail.com>
2019-07-08 09:20:12 +0200

    uploading files

---
+++ BasicSynthesizer/BasicSynthesizer.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace BasicSynthesizer
+{
+    partial class Synth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oscillator1 = new BasicSynthesizer.Oscillator();
+            this.SuspendLayout();
+            // 
+            // oscillator1
+            // 
+            this.oscillator1.Location = new System.Drawing.Point(13, 13);
+            this.oscillator1.Name = "oscillator1";
+            this.oscillator1.Size = new System.Drawing.Size(298, 130);
+            this.oscillator1.TabIndex = 0;
+            this.oscillator1.TabStop = false;
+            // 
+            // Synth
+            // 
+            this.ClientSize = new System.Drawing.Size(323, 289);
+            this.Controls.Add(this.oscillator1);
+            this.Name = "Synth";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BasicSynthesizer_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private Oscillator oscillator1;
+    }
+}
+
---
+++ BasicSynthesizer/BasicSynthesizer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Windows.Forms;
+
+namespace BasicSynthesizer
+{
+    public partial class Synth : Form
+    {
+        private const int SAMPLE_RATE = 41000;
+        private const short BITS_PER_SAMPLE = 16;
+
+        public Synth()
+        {
+            InitializeComponent();
+        }
+
+        private void BasicSynthesizer_KeyDown(object sender, KeyEventArgs e)
+        {
+            short[] wave = new short[SAMPLE_RATE];
+            byte[] binaryWave = new byte[SAMPLE_RATE * sizeof(short)];
+            float frequency = 440f;
+            int samplesPerWaveLength = (int)(SAMPLE_RATE / frequency);
+            short ampStep = (short)((short.MaxValue * 2) / samplesPerWaveLength);
+            short tempSample;
+
+            foreach (Oscillator oscillator in this.Controls.OfType<Oscillator>())
+            {
+                switch (oscillator.WaveForm)
+                {
+                    case WaveForm.Sine:
+                        for (int i = 0; i < SAMPLE_RATE; i++)
+                        {
+                        wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin((2 * Math.PI * frequency / SAMPLE_RATE) * i));
+                        }
+                        break;
+
+                    case WaveForm.Square:
+                        for (int i = 0; i < SAMPLE_RATE; i++)
+                        {
+                            wave[i] = Convert.ToInt16(short.MaxValue * Math.Sign(Math.Sin((2 * Math.PI * frequency / SAMPLE_RATE) * i)));
+                        }
+                        break;
+
+                    case WaveForm.Saw:
+                        for (int i = 0; i < SAMPLE_RATE; i++)
+                        {
+                            tempSample = -short.MaxValue;
+                            for (int j = 0; j < samplesPerWaveLength && i < SAMPLE_RATE; j++)
+                            {
+                                tempSample += ampStep;
+                                wave[i++] = Convert.ToInt16(tempSample);
+
+                            }
+                            i--;
+                        }
+                        break;
+
+                    case WaveForm.Triangle:
+                        tempSample = -short.MaxValue;
+                        for (int i = 0; i < SAMPLE_RATE; i++)
+                        {
+                            if (Math.Abs(tempSample + ampStep) > short.MaxValue)
+                            {
+                                ampStep = (short)-ampStep;
+                            }
+                            tempSample += ampStep;
+                            wave[i] = Convert.ToInt16(tempSample);
+                        }
+                        break;
+
+                    case WaveForm.Noise:
+                        Random rnd = new Random();
+                        for (int i = 0; i < SAMPLE_RATE; i++)
+                        {
+                            wave[i] = (short)rnd.Next(-short.MaxValue, short.MinValue);
+                        }
+
+                        break;
+                }
+            
+            }
+            
+            Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
+            {
+                int blockAlign = BITS_PER_SAMPLE / 8;
+                int numOfChannels = 1;
+                int subChunkTwoSize = SAMPLE_RATE * numOfChannels * blockAlign;
+                binaryWriter.Write(new[] { 'R', 'I', 'F', 'F' });
+                binaryWriter.Write(36 + subChunkTwoSize);
+                binaryWriter.Write(new[] { 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ' });
+                binaryWriter.Write(16);
+                binaryWriter.Write((short)1);
+                binaryWriter.Write((short)numOfChannels);
+                binaryWriter.Write(SAMPLE_RATE);
+                binaryWriter.Write(SAMPLE_RATE * blockAlign);
+                binaryWriter.Write((short)blockAlign);
+                binaryWriter.Write(BITS_PER_SAMPLE);
+                binaryWriter.Write(new[] { 'd', 'a', 't', 'a' });
+                binaryWriter.Write(subChunkTwoSize);
+                binaryWriter.Write(binaryWave);
+                memoryStream.Position = 0;
+                new SoundPlayer(memoryStream).Play();
+            }
+        }   
+    }
+
+    public enum WaveForm
+    {
+        Sine, Square, Saw, Triangle, Noise
+    }
+}
